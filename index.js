ac563fadcad916215c37b874329eb2efdf9b6f62
Rahat Ahmed <rahatarmanahmed@gmail.com>
2017-02-06 05:37:28 -0600

    Listen to localhost by default

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const pino = require('pino')()
 
 const db = level('./.db', { valueEncoding: 'json' })
 const port = process.env.PORT || 3000
+const host = process.env.HOST || '127.0.0.1'
 
 try {
   const server = micro(require('./server/app')({ db, pino }))
-  server.listen(port)
+  server.listen(port, host)
   pino.info(`Server started on port ${port}`)
 } catch (err) {
   pino.error(err, 'Failed to start server')
